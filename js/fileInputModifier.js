13a780dfd4cf0560d79677c044e8c391cf56422a
EvgenRomanov98 <evrmo98@gmail.com>
2019-05-15 04:14:34 +0300

    finish

--- js/fileInputModifier.js
+++ js/fileInputModifier.js
@@ -1,54 +1,84 @@
-var fileInput1 = document.getElementById("account--payment"),
-    fileInput2 = document.getElementById("medical--history"),
-    fileInput3 = document.getElementById("birth--certificate"),
-    fileInput4 = document.getElementById("passport"),
-    fileInput5 = document.getElementById("ITN");
+window.onload = function () {
+    var fileInput1 = document.getElementById("account--payment"),
+        fileInput2 = document.getElementById("medical--history"),
+        fileInput3 = document.getElementById("birth--certificate"),
+        fileInput4 = document.getElementById("passport"),
+        fileInput5 = document.getElementById("ITN"),
+        fileInput6 = document.getElementById("request--help"),
+        fileInput7 = document.getElementById("financial-statements"),
+        fileInput8 = document.getElementById("request--help3"),
+        fileInput9 = document.getElementById("financial-statements3");
 
-pathFileList = document.getElementsByClassName("tc__p--input-file-value");
+    // selectNeedHelpPage = document.getElementById("select-need-help-page"),
+    // headerLabelNeedHelp = document.getElementById("header__label--need-help"),
+    // mainSectionRight = document.getElementsByClassName("main__section--right");
 
-var pathFile1 = pathFileList.item(0);
-var pathFile2 = pathFileList.item(1);
-var pathFile3 = pathFileList.item(2);
-var pathFile4 = pathFileList.item(3);
-var pathFile5 = pathFileList.item(4);
+    var pathFileList = document.getElementsByClassName("tc__p--input-file-value");
 
-fileInput1.addEventListener("change", function (event) {
-    pathFile1.innerHTML = this.value;
-});
+    var pathFile1 = pathFileList.item(0);
+    var pathFile2 = pathFileList.item(1);
+    var pathFile3 = pathFileList.item(2);
+    var pathFile4 = pathFileList.item(3);
+    var pathFile5 = pathFileList.item(4);
+    var pathFile6 = pathFileList.item(5);
+    var pathFile7 = pathFileList.item(6);
+    var pathFile8 = pathFileList.item(7);
+    var pathFile9 = pathFileList.item(8);
 
-fileInput2.addEventListener("change", function (event) {
-    pathFile2.innerHTML = this.value;
-});
+    // if (selectNeedHelpPage.checked) {
+    //     headerLabelNeedHelp.style.color = "#24bb13";
+    // } else {
+    //     headerLabelNeedHelp.style.color = "#424242";
+    // }
 
-fileInput3.addEventListener("change", function (event) {
-    pathFile3.innerHTML = this.value;
-});
+    fileInput1.addEventListener("change", function (event) {
+        pathFile1.innerHTML = this.value;
+    });
 
-fileInput4.addEventListener("change", function (event) {
-    pathFile4.innerHTML = this.value;
-});
+    fileInput2.addEventListener("change", function (event) {
+        pathFile2.innerHTML = this.value;
+    });
 
-fileInput5.addEventListener("change", function (event) {
-    pathFile5.innerHTML = this.value;
-});
+    fileInput3.addEventListener("change", function (event) {
+        pathFile3.innerHTML = this.value;
+    });
 
+    fileInput4.addEventListener("change", function (event) {
+        pathFile4.innerHTML = this.value;
+    });
 
-// var fileInputList = document.getElementsByClassName("input--file"),
-//     pathFileList = document.querySelector("tc__p--input-file-value");
-//
-// console.log(fileInputList.length);
-//
-// for (var i = 0; i < fileInputList.length; i++) {
-//     console.log(i);
-//     fileInputList[i].addEventListener("change", function (event) {
-//         console.log(i);
-//         pathFileList[i].innerHTML = event.value;
-//     });
-// }
-// fileInputList.forEach(function (item, i, fileInputList) {
-//     console.log( i + ": " + item + " (массив:" + arr + ")" );
-// });
-// fileInput.addEventListener("change", function (event) {
-//     debugger;
-//     pathFile.innerHTML = this.value;
-// });
+    fileInput5.addEventListener("change", function (event) {
+        pathFile5.innerHTML = this.value;
+    });
+
+    fileInput6.addEventListener("change", function (event) {
+        pathFile6.innerHTML = this.value;
+    });
+
+    fileInput7.addEventListener("change", function (event) {
+        pathFile7.innerHTML = this.value;
+    });
+    fileInput8.addEventListener("change", function (event) {
+        pathFile8.innerHTML = this.value;
+    });
+    fileInput9.addEventListener("change", function (event) {
+        pathFile9.innerHTML = this.value;
+    });
+    // selectNeedHelpPage.addEventListener("change", function () {
+    //     if (selectNeedHelpPage.checked) {
+    //         headerLabelNeedHelp.style.color = "#24bb13";
+    //     }
+    //     if (selectNeedHelpPage.disabled){
+    //         headerLabelNeedHelp.style.color = "#424242";
+    //     }
+    // });
+    //
+    // mainSectionRight[0].addEventListener("touchstart", function () {
+    //     if (selectNeedHelpPage.checked) {
+    //         headerLabelNeedHelp.style.color = "#24bb13";
+    //     }
+    //     if (selectNeedHelpPage.disabled){
+    //         headerLabelNeedHelp.style.color = "#424242";
+    //     }
+    // });
+};
---
+++ js/program-fund-select-Page.js
@@ -0,0 +1,16 @@
+// var currentProgram = document.getElementsByClassName("pf__main--current-program");
+// var finishedProgram = document.getElementsByClassName("pf__main--finished-program");
+// var currentInputCheckbox = document.getElementById("input--current");
+// var finisedInputCheckbox = document.getElementById("input--finished");
+//
+// currentInputCheckbox.addEventListener("click", function () {
+//     currentProgram[0].style.display = "block";
+//     finishedProgram[0].style.display = "none";
+// });
+//
+// finisedInputCheckbox.addEventListener("click", function () {
+//     currentProgram[0].style.display = "none";
+//     finishedProgram[0].style.display = "block";
+// });
+//
+//
